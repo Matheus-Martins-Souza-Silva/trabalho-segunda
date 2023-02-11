9b790b0f22a80fe44c0e9447044c7218746932a9
Fade68 <lspectre4@gmail.com>
2023-02-11 04:05:49 -0300

    Angular 2.0

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'trabalho-segunda'`, () => {
+  it(`should have as title 'Pagina_da_Academia'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('trabalho-segunda');
+    expect(app.title).toEqual('Pagina_da_Academia');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('trabalho-segunda app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Pagina_da_Academia app is running!');
   });
 });
